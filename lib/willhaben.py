cd00d8d42ed379b4e1914b6cffb1bc19628bab06
erstert3st <lura12111@gmail.com>
2024-04-06 21:12:05 +0200

    fuck formatter

--- lib/willhaben.py
+++ lib/willhaben.py
@@ -6,18 +6,9 @@ from typing import List, Optional, Literal, Union
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
 import json
 from http import HTTPStatus
-from lib.query_params import (
-    PROPERTY_TYPE,
-    FREE_AREA_TYPE,
-    ESTATE_PREFERENCE,
-    AVAILABILITY,
-    CATEGORY,
-    STATE,
-    DISTRICT,
-)
+from lib.query_params import PROPERTY_TYPE, FREE_AREA_TYPE, ESTATE_PREFERENCE, AVAILABILITY, CATEGORY, STATE, DISTRICT
 from lib.helpers import handle_argument_list
 
 
@@ -37,17 +28,10 @@ class WillhabenQueryBuilder:
         self.size_from: Optional[int] = None
         self.size_to: Optional[int] = None
         self.number_of_rooms: Optional[List[str]] = []  # 1,2,3,4,5,6-9,10+
-        self.property_types: Optional[List[PROPERTY_TYPE]] = (
-            []
-        )  # Immobilientyp. if none is selected, the result will contain all types
-        self.free_area_types: Optional[List[FREE_AREA_TYPE]] = (
-            []
-        )  # Freiflächen. if none is selected, the result will contain all types
-        self.estate_preferences: Optional[List[ESTATE_PREFERENCE]] = (
-            []
-        )  # Ausstattung. if none is selected, the result will contain all types
+        self.property_types: Optional[List[PROPERTY_TYPE]] = []  # Immobilientyp. if none is selected, the result will contain all types
+        self.free_area_types: Optional[List[FREE_AREA_TYPE]] = []  # Freiflächen. if none is selected, the result will contain all types
+        self.estate_preferences: Optional[List[ESTATE_PREFERENCE]] = []  # Ausstattung. if none is selected, the result will contain all types
         self.availabe_now: bool = None
-        self.ua_header = {"User-Agent": UserAgent().random}
 
     def set_category(self, category: CATEGORY):
         self.category = category
@@ -59,73 +43,69 @@ class WillhabenQueryBuilder:
 
     def set_district(self, district: DISTRICT):
         if self.state is None:
-            raise Exception("You must set the state first.")
+            raise Exception('You must set the state first.')
         self.district = district
         return self
 
     def set_rows(self, rows: int):
         if rows < 1:
-            raise Exception("Rows must be greater than 0.")
+            raise Exception('Rows must be greater than 0.')
         self.rows = rows
         return self
 
     def set_price_from(self, price_from: int):
         if price_from < 1:
-            raise Exception("Price from must be greater than 0.")
+            raise Exception('Price from must be greater than 0.')
         self.price_from = price_from
         return self
 
     def set_price_to(self, price_to: int):
         if price_to < 1:
-            raise Exception("Price to must be greater than 0.")
+            raise Exception('Price to must be greater than 0.')
         self.price_to = price_to
         return self
 
     def set_size_from(self, size_from: int):
         if size_from < 1:
-            raise Exception("Size from must be greater than 0.")
+            raise Exception('Size from must be greater than 0.')
         self.size_from = size_from
         return self
 
     def set_size_to(self, size_to: int):
         if size_to < 1:
-            raise Exception("Size to must be greater than 0.")
+            raise Exception('Size to must be greater than 0.')
         self.size_to = size_to
         return self
 
-    def add_number_of_rooms(self, num: Literal["1", "2", "3", "4", "5", "6-9", "10+"]):
-        if num not in ["1", "2", "3", "4", "5", "6-9", "10+"]:
-            raise ValueError(
-                "Invalid number of rooms. Must be 1, 2, 3, 4, 5, 6-9 or 10+ in datatype str."
-            )
-        if num in ["1", "2", "3", "4", "5"]:
-            self.number_of_rooms.append(f"{num}X{num}")
-        elif num == "6-9":
-            self.number_of_rooms.append("6X9")
-        elif num == "10+":
-            self.number_of_rooms.append("10X")
+    def add_number_of_rooms(self, num: Literal['1', '2', '3', '4', '5', '6-9', '10+']):
+        if num not in ['1', '2', '3', '4', '5', '6-9', '10+']:
+            raise ValueError('Invalid number of rooms. Must be 1, 2, 3, 4, 5, 6-9 or 10+ in datatype str.')
+        if num in ['1', '2', '3', '4', '5']:
+            self.number_of_rooms.append(f'{num}X{num}')
+        elif num == '6-9':
+            self.number_of_rooms.append('6X9')
+        elif num == '10+':
+            self.number_of_rooms.append('10X')
         return self
 
     @handle_argument_list
-    def add_property_type(
-        self, property_type: Union[PROPERTY_TYPE, List[PROPERTY_TYPE]]
-    ):
+    def add_property_type(self, property_type: Union[PROPERTY_TYPE, List[PROPERTY_TYPE]]):
         if property_type not in PROPERTY_TYPE:
-            raise ValueError("Invalid property type.")
+            raise ValueError('Invalid property type.')
         self.property_types.append(property_type)
         return self
 
     @handle_argument_list
     def add_free_area_type(self, free_area_type: FREE_AREA_TYPE):
         if free_area_type not in FREE_AREA_TYPE:
-            raise ValueError("Invalid free area type.")
+            raise ValueError('Invalid free area type.')
         self.free_area_types.append(free_area_type)
         return self
 
     @handle_argument_list
     def add_estate_preference(self, estate_preference: ESTATE_PREFERENCE):
         if estate_preference not in ESTATE_PREFERENCE:
-            raise ValueError("Invalid estate preference.")
+            raise ValueError('Invalid estate preference.')
         self.estate_preferences.append(estate_preference)
         return self
 
@@ -137,37 +117,37 @@ class WillhabenQueryBuilder:
     def get_query_url(self):
         self._query_url = self._base_url
         if self.category is None:
-            raise Exception("You must set the category before getting the query url.")
-        self._query_url += f"/{self.category.value}"
+            raise Exception('You must set the category before getting the query url.')
+        self._query_url += f'/{self.category.value}'
         if self.state is not None:
-            self._query_url += f"/{self.state.value}"
+            self._query_url += f'/{self.state.value}'
         if self.district is not None and self.state is None:
-            raise Exception("You must set the state before setting the district.")
+            raise Exception('You must set the state before setting the district.')
         elif self.district is not None and self.state is not None:
-            self._query_url += f"/{self.district.value}"
-        self._query_url += f"?rows={self.rows}"
+            self._query_url += f'/{self.district.value}'
+        self._query_url += f'?rows={self.rows}'
         if self.price_from is not None:
-            self._query_url += f"&PRICE_FROM={self.price_from}"
+            self._query_url += f'&PRICE_FROM={self.price_from}'
         if self.price_to is not None:
-            self._query_url += f"&PRICE_TO={self.price_to}"
+            self._query_url += f'&PRICE_TO={self.price_to}'
         if self.size_from is not None:
-            self._query_url += f"&ESTATE_SIZE/LIVING_AREA_FROM={self.size_from}"
+            self._query_url += f'&ESTATE_SIZE/LIVING_AREA_FROM={self.size_from}'
         if self.size_to is not None:
-            self._query_url += f"&ESTATE_SIZE/LIVING_AREA_TO={self.size_to}"
+            self._query_url += f'&ESTATE_SIZE/LIVING_AREA_TO={self.size_to}'
         if self.number_of_rooms is not None:
             for num in self.number_of_rooms:
-                self._query_url += f"&NO_OF_ROOMS_BUCKET={num}"
+                self._query_url += f'&NO_OF_ROOMS_BUCKET={num}'
         if self.property_types is not None:
             for property_type in self.property_types:
-                self._query_url += f"&PROPERTY_TYPE={property_type.value}"
+                self._query_url += f'&PROPERTY_TYPE={property_type.value}'
         if self.free_area_types is not None:
             for free_area_type in self.free_area_types:
-                self._query_url += f"&FREE_AREA/FREE_AREA_TYPE={free_area_type.value}"
+                self._query_url += f'&FREE_AREA/FREE_AREA_TYPE={free_area_type.value}'
         if self.estate_preferences is not None:
             for estate_preference in self.estate_preferences:
-                self._query_url += f"&ESTATE_PREFERENCE={estate_preference.value}"
+                self._query_url += f'&ESTATE_PREFERENCE={estate_preference.value}'
         if self.availabe_now is not None:
-            self._query_url += f"&AVAILABLETODAY={AVAILABILITY.AVAILABLE_NOW.value}"
+            self._query_url += f'&AVAILABLETODAY={AVAILABILITY.AVAILABLE_NOW.value}'
         return self._query_url
 
     def getRandomHeader(self):
@@ -199,36 +179,43 @@ class WillhabenQueryBuilder:
                     print(f"Error decoding JSON: {e}")
         print(f"ERROR max pages reached: {self.max_pages}")
 
-#lol make bugfix on there git xD 
     # This method returns the listings as an array of json objects with only a selection of attributes.
     def get_formatted_listings_as_json(self,listings ) -> List[dict]:
      #   listings = self.get_full_listings_as_json()
         listings_json = []
         for listing in listings:
-            attributes = listing["attributes"]["attribute"]
+            attributes = listing['attributes']['attribute']
             property_type = number_of_rooms = published_date = price = size_sqm = ""
             for attribute in attributes:
-                if attribute["name"] == "PROPERTY_TYPE":
-                    property_type = attribute["values"][0]
-                if attribute["name"] == "NUMBER_OF_ROOMS":
-                    number_of_rooms = attribute["values"][0]
-                if attribute["name"] == "PUBLISHED_String":
-                    published_date_str = attribute["values"][0]
+                if attribute['name'] == 'PROPERTY_TYPE':
+                    property_type = attribute['values'][0]
+                if attribute['name'] == 'NUMBER_OF_ROOMS':
+                    number_of_rooms = attribute['values'][0]
+                if attribute['name'] == 'PUBLISHED_String':
+                    published_date_str = attribute['values'][0]
                     date_format = "%Y-%m-%dT%H:%M:%SZ"
                     try:
-                        published_date = datetime.strptime(
-                            published_date_str, date_format
-                        ).isoformat()
+                        published_date = datetime.strptime(published_date_str, date_format).isoformat()
                     except ValueError:
                         print(f"Error parsing date: {published_date_str}")
                         published_date = None
-                if attribute["name"] == "PRICE_FOR_DISPLAY":
-                    price = attribute["values"][0]
-                if attribute["name"] == "ESTATE_SIZE":
-                    size_sqm = attribute["values"][0]
-                if attribute["name"] == "SEO_URL":
-                    url = attribute["values"][0]
-            formatted_listing = {}
-            
+                if attribute['name'] == 'PRICE_FOR_DISPLAY':
+                    price = attribute['values'][0]
+                if attribute['name'] == 'ESTATE_SIZE':
+                    size_sqm = attribute['values'][0]
+                if attribute['name'] == 'SEO_URL':
+                    url = attribute['values'][0]
+            formatted_listing = {
+                'id': listing['id'],
+                'platform': 'willhaben',
+                'description': listing['description'],
+                'property_type': property_type,
+                'number_of_rooms': number_of_rooms,
+                'date_published': published_date,
+                'price_per_month': price,
+                'size_sqm': size_sqm,
+                'url': "https://www.willhaben.at/iad/" + url
+
+            }
             listings_json.append(formatted_listing)
        # return listings_json
