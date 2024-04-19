0249cfaef003e6ac5dc4303d88443d0d4892180d
erstert3st <lura12111@gmail.com>
2024-04-19 23:08:19 +0200

    zc

--- map/src/app/map-config/map-config.component.ts
+++ map/src/app/map-config/map-config.component.ts
@@ -25,6 +25,13 @@ export class MapConfigComponent {
     ]
   };
 
+  public generateSql() {
+    let sqlQuery = 'SELECT * FROM table WHERE ';
+    const conditions = this.query.rules.map(rule => `${rule.field} ${rule.operator} ${rule.value}`);
+    sqlQuery += conditions.join(` ${this.query.condition} `);
+    console.log(sqlQuery);
+  }
+
   config: QueryBuilderConfig = {
     fields: {
       property_type: { name: 'Typ', type: 'string' },
@@ -53,58 +60,32 @@ gender: {
       }
 
 */
-  classNames: QueryBuilderClassNames = {
-    removeIcon: "fa fa-minus",
-    addIcon: "fa fa-plus",
-    arrowIcon: "fa fa-chevron-right px-2",
-    button: "btn",
-    buttonGroup: "btn-group",
-    rightAlign: "order-12 ml-auto",
-    switchRow: "d-flex px-2",
-    switchGroup: "d-flex align-items-center",
-    switchRadio: "custom-control-input",
-    switchLabel: "custom-control-label",
-    switchControl: "custom-control custom-radio custom-control-inline",
-    row: "row p-2 m-1",
-    rule: "border",
-    ruleSet: "border",
-    invalidRuleSet: "alert alert-danger",
-    emptyWarning: "text-danger mx-auto",
-    operatorControl: "form-control",
-    operatorControlSize: "col-auto pr-0",
-    fieldControl: "form-control",
-    fieldControlSize: "col-auto pr-0",
-    entityControl: "form-control",
-    entityControlSize: "col-auto pr-0",
-    inputControl: "form-control",
-    inputControlSize: "col-auto"
-  };
-/* 
-  classNames: QueryBuilderClassNames = {
-    removeIcon: "fa fa-minus",
-    addIcon: "fa fa-plus",
-    arrowIcon: "fa fa-chevron-right px-2",
-    button: "btn",
-    buttonGroup: "btn-group",
-    rightAlign: "order-12 ml-auto",
-    switchRow: "d-flex px-2",
-    switchGroup: "d-flex align-items-center",
-    switchRadio: "custom-control-input",
-    switchLabel: "custom-control-label",
-    switchControl: "custom-control custom-radio custom-control-inline",
-    row: "row p-2 m-1",
-    rule: "border",
-    ruleSet: "border",
-    invalidRuleSet: "alert alert-danger",
-    emptyWarning: "text-danger mx-auto",
-    operatorControl: "form-control",
-    operatorControlSize: "col-auto pr-0",
-    fieldControl: "form-control",
-    fieldControlSize: "col-auto pr-0",
-    entityControl: "form-control",
-    entityControlSize: "col-auto pr-0",
-    inputControl: "form-control",
-    inputControlSize: "col-auto"
-  }; */
+public bootstrapClassNames: QueryBuilderClassNames = {
+  removeIcon: 'fa fa-minus',
+  addIcon: 'fa fa-plus',
+  arrowIcon: 'fa fa-chevron-right px-2',
+  button: 'btn',
+  buttonGroup: 'btn-group',
+  rightAlign: 'order-12 ml-auto',
+  switchRow: 'd-flex px-2',
+  switchGroup: 'd-flex align-items-center',
+  switchRadio: 'custom-control-input',
+  switchLabel: 'custom-control-label',
+  switchControl: 'custom-control custom-radio custom-control-inline',
+  row: 'row p-2 m-1',
+  rule: 'border',
+  ruleSet: 'border',
+  invalidRuleSet: 'alert alert-danger',
+  emptyWarning: 'text-danger mx-auto',
+  operatorControl: 'form-control',
+  operatorControlSize: 'col-auto pr-0',
+  fieldControl: 'form-control',
+  fieldControlSize: 'col-auto pr-0',
+  entityControl: 'form-control',
+  entityControlSize: 'col-auto pr-0',
+  inputControl: 'form-control',
+  inputControlSize: 'col-auto'
+};
+
 
 }
