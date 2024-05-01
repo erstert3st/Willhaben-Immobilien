031a4f757ae7f9b71c599ae74695c8ad0832cc01
erstert3st <erstert3st@email.no>
2024-05-01 22:53:08 +0200

    -.-"

--- map/src/app/error-handler-dialog/error-handler-dialog.component.ts
+++ map/src/app/error-handler-dialog/error-handler-dialog.component.ts
@@ -1,11 +1,24 @@
 import { Component, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 
+
 @Component({
   selector: 'error-handler-dialog',
   standalone: true,
   imports: [],
-  template: './error-handler-dialog.component.html',
+  template: `<div class="modal-dialog modal-dialog-centered onTop">
+  <div class="modal-content bg-dark text-white">
+    <div class="modal-header p-3">
+      <h5 class="modal-title">{{ title }}</h5> <!-- Verwenden Sie h5 anstelle von h1, um den Titel kleiner zu machen -->
+    </div>
+    <div class="modal-body text-center p-3"> 
+      <p class="small">{{ data.message }}</p> <!-- Verwenden Sie die small Klasse, um den Text kleiner zu machen -->
+    </div>
+    <div class="modal-footer justify-content-end p-2">
+      <button id="error-btn" class="btn btn-light" data-dismiss="modal">Okay</button>
+    </div>
+  </div>
+</div>`,
   styleUrl: './error-handler-dialog.component.css'
 })
 
